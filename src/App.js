c75b933b8780f4b691e9d93aca86b74c0522c4e8
ihsan <ihsanrawi@gmail.com>
2020-05-26 10:07:33 +0800

    Remove traces of using props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import MusicJs from "./components/index";
 
-const initialState = {
-	debug: false,
-	// debug: true,
-	isFullScreen: false,
-	// isFullScreen: true,
-	isPlaying: false,
-	header: {
-		titleStack: ["Library", "Artists", "Albums", "Playlists"],
-	},
-	playerConfig: {
-		volume: 6,
-	},
-	miniplayer: {
-		artwork: "images/default_artwork.svg",
-		title: "Music Player",
-		artist: "Ihsan Rawi",
-		album: "ReactJs",
-	},
-};
-
 function App() {
-	const [defaultState, setdefaultState] = useState(initialState);
 	return (
 		<div>
-			<MusicJs musicJs={defaultState} />
+			<MusicJs />
 		</div>
 	);
 }
