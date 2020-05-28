7d0138980f81d3b6080e6f501a11a3f6dc3b887c
Ihsan <ihsanrawi@gmail.com>
2020-05-28 16:36:34 +0800

    Initial commit of button

---
+++ src/components/commons/button/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const ButtonContainer = styled.div``;
+
+function Button() {
+	return <ButtonContainer></ButtonContainer>;
+}
+
+export default Button;
