294c70d8285f33824ae74a69feb3572904af185e
Ihsan <ihsanrawi@gmail.com>
2020-05-30 14:02:48 +0800

    initial commit of artist-list

---
+++ src/components/page/artist-list/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
+
+import Button from "../../commons/button";
+import { pushView } from "../../../redux/actions/view";
+
+const Container = styled.div`
+	margin-top: 48px;
+`;
+
+const ButtonContainer = styled.div``;
+
+export const ArtistsView = ({ viewState, pushView }) => {
+	const changeView = (name) => {
+		pushView({
+			name,
+			props: {},
+		});
+	};
+
+	return (
+		<Container>
+			<ButtonContainer>
+				<Button
+					label="Artists"
+					theme="red"
+					chevron
+					onClick={() => changeView("Artists")}
+				/>
+				<Button
+					label="Albums"
+					theme="red"
+					chevron
+					onClick={() => changeView("Albums")}
+				/>
+			</ButtonContainer>
+		</Container>
+	);
+};
+
+const mapStateToProps = (state) => ({
+	navState: state.navState,
+	viewState: state.viewState,
+});
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		pushView: (view) => dispatch(pushView(view)),
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArtistsView);
