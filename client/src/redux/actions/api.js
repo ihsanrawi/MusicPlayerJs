abae93bbb47bd57fe4279a83fbb3caca38939abc
Ihsan <ihsanrawi@gmail.com>
2020-05-30 15:31:25 +0800

    Move client files into client folder

---
+++ client/src/redux/actions/api.js
@@ -0,0 +1,11 @@
+import {
+	FETCH_ALBUM,
+	FETCH_ALBUM_LIST,
+	FETCH_ARTIST,
+	FETCH_ARTIST_LIST,
+} from "../types";
+
+export const fetchArtist = () => {};
+export const fetchArtists = () => {};
+export const fetchAlbum = () => {};
+export const fetchAlbums = () => {};
