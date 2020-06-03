41c509319da1451b06221db9b0b49bf781293957
Ihsan <ihsanrawi@gmail.com>
2020-06-03 16:01:02 +0800

    test api get token

--- routes/api/spotify.js
+++ routes/api/spotify.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const SpotifyWebApi = require("spotify-web-api-node");
-
 const router = express.Router();
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const credentials = {
 	clientId: "3dd88d24614f4d6b9e0c79efd4d7782c",
@@ -13,20 +12,26 @@ const spotifyApi = new SpotifyWebApi(credentials);
 // @route    POST api/posts
 // @desc     Create a post
 // @access   Private
-const getAccessToken = spotifyApi
-	.clientCredentialsGrant()
-	.then((res) => {
-		console.log("The access token expires in " + res.body["expires_in"]);
-		console.log("The access token is " + res.body["access_token"]);
+router.get("/", (req, res) => {
+	const getAccessToken = spotifyApi
+		.clientCredentialsGrant()
+		.then((res) => {
+			console.log(
+				"The access token expires in " + res.body["expires_in"]
+			);
 
-		// Save the access token so that it's used in future calls
-		spotifyApi.setAccessToken(res.body["access_token"]);
-	})
-	.catch((err) => {
-		console.log(
-			"Something went wrong when retrieving an access token",
-			err
-		);
-	});
+			console.log("The access token is " + res.body["access_token"]);
+
+			// Save the access token so that it's used in future calls
+			spotifyApi.setAccessToken(res.body["access_token"]);
+			return res.statusCode(200).json(res.body["access_token"]);
+		})
+		.catch((err) => {
+			console.log(
+				"Something went wrong when retrieving an access token",
+				err
+			);
+		});
+});
 
 module.exports = router;
